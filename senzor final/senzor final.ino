f733b74f937862e75e08e76913df4a6b3ab14671
Peter Maxin <petermaxin546@gmail.com>
2024-01-26 16:19:53 +0100

    Upravil som senzor xd

--- senzor final/senzor final.ino
+++ senzor final/senzor final.ino
@@ -1,6 +1,6 @@
-#define MOISTURE_PIN 4 // ESP32 pin GPIO36 (ADC0) that connects to AOUT pin of moisture sensor
-int WET_VALUE = 1000;  // Hodnota úplne mokrého senzora
-int DRY_VALUE = 2750; // Hodnota úplne suchého senzora
+#define MOISTURE_PIN 4
+int WET_VALUE = 2400;  // Hodnota úplne mokrého senzora
+int DRY_VALUE = 4095; // Hodnota úplne suchého senzora
 int ledNum = 5; // Počet LEDiek
 int step = (DRY_VALUE - WET_VALUE)/ledNum; //Vypočítanie jedného kroku (podľa počtu LEDiek)
 int myPins[5] = {13, 26, 32, 23, 19}; //Pole s pinami čo sa používajú
@@ -14,12 +14,13 @@ void setup() {
 }
 
 void loop() {
-  int timesContains = -1; // Ideme od -1 kvôli cyklu ktorý zapína LEDky. Ak by sme išli od 0, prvá LEDka by sa zapla, aj keď by sa nemala, lebo je 0-tá v poli (išli by sme od 0 do 0, t.j. vrátane nuly)
-  int value = analogRead(MOISTURE_PIN); // read the analog value from soild moisture sensor
+  int timesContains = 0; // Ideme od -1 kvôli cyklu ktorý zapína LEDky. Ak by sme išli od 0, prvá LEDka by sa zapla, aj keď by sa nemala, lebo je 0-tá v poli (išli by sme od 0 do 0, t.j. vrátane nuly)
+  int value = analogRead(MOISTURE_PIN);
+  value = DRY_VALUE - value;
 
   for(int i = 1; ; i++) // ideme od 1 kvôli tomu, že step * 0 bude stále väčšie ako akékoľvek kladné číslo.
   {
-    if(value - (step * i) >= step) // Dokiaľ je nameraná hodnota - (krok * i [premenná v cykle]) väčšia alebo rovná ako krok, pripočítava sa k
+    if(value - (step * i) >= 0) // Dokiaľ je nameraná hodnota - (krok * i [premenná v cykle]) väčšia alebo rovná ako 0, pripočítava sa k
     {
       timesContains++;
     }
@@ -28,7 +29,7 @@ void loop() {
       break; //Vyjde sa z cyklu keď by nameraná hodnota bola menšia ako krok
     }
   }
-
+  
   for (int i = 0; i < timesContains; i++){
     digitalWrite(myPins[i], HIGH); //Zapneme toľko pinov z poľa deklarovaného vyššie, koľko sme napočítali vyššie
   }
@@ -40,5 +41,4 @@ void loop() {
   Serial.print(" (");
   Serial.print(value);
   Serial.println(")");
-
 }
